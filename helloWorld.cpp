636507e0473dc8d84359ce40742e7c452abed3dd
dark nova <102454319+darkNovaV1@users.noreply.github.com>
2024-02-14 17:43:33 +0530

    Create helloWorld.cpp
    This is my first C++ Program to test my C++ environment

---
+++ helloWorld.cpp
@@ -0,0 +1,16 @@
+// This program demonstrates a simple "Hello World" output in C++.
+
+// Include the input/output stream library
+#include <iostream>
+
+// Use the standard namespace for convenience
+using namespace std;
+
+// The main function, where program execution begins
+int main() {
+    // Print "Hello World!" to the console
+    cout << "Hello World!";
+
+    // Indicate successful execution by returning 0
+    return 0;
+}
